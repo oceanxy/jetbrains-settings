1ec331dfc97642db79eaa7d368c3fae8c90bc052
Oceanxy <xyzsyx@163.com>
2019-08-16 14:15:50 +0800

    WS-2019.2 <oceanxy@Oceanxy-HY.local Create git_toolbox.xml, git_toolbox_2.xml, project.default.xml, Default copy.xml, yiiguxing.translation.xml, Default.xml, file.template.settings.xml, ignore.xml, find.xml, SQL Script.sql, textmate_os.xml, _@user_Material Oceanic.icls, KeyPromoterXSettings.xml, _@user_Darcula.icls, path.macros.xml, JavaScript File.js, XHTML File.xhtml, Default.xml, docker.xml, gitee.xml, Default.xml, encoding.xml, editor.codeinsight.xml, keymap.xml, editor.xml, applicationLibraries.xml, markdown_navigator.shared.xml, coding_settings.xml, debugger.xml, HTML4 File.html, updates.xml, baseRefactoring.xml, ide.general.xml, _@user_Default.icls, gitlab-project-settings-new-format.xml, rainbow_brackets.xml, user.xml, git.xml, HTML File.html, ui.lnf.xml, filetypes.xml, projectView.xml, SvnToolBox.xml, plugin_ui.xml, watcherDefaultTasks.xml, colors.scheme.xml, notifications.xml, github.xml, emmet.xml, material_custom_theme.xml, parameter.hints.xml, laf.xml, web-browsers.xml, material_theme.xml, dbnavigator.xml, tasks.xml, vcs.xml, usageView.xml, Mac OS X copy.xml

---
+++ fileTemplates/internal/JavaScript File.js
@@ -0,0 +1,10 @@
+/**
+ * @Author: Oceanxy
+ * @Email: xieyang@hiynn.com
+ * @Description: 
+ * @Date: $DATE
+ * @LastModified: Oceanxy
+ * @LastModifiedTime: $DEFAULT_RETURN_VALUE
+ */
+ 
+ #if ( $RETURN_TYPE != "any" && $RETURN_TYPE != "void" )return $DEFAULT_RETURN_VALUE;#end
